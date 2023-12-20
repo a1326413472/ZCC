4b966f1493c51314e54e965e4d71af0ab41a9c5e
BaTianPig <1326413472@qq.com>
2023-12-20 23:21:40 +0800

    feature:1.0

---
+++ Source/ACC/Public/ZCCCoreTypes.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ZCCCoreTypes.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct ACC_API FZCCParam : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FZCCParam(FName InKey, FName InCustomInfo) : Key(InKey), CustomInfo(InCustomInfo)
+	{
+	}
+
+	FZCCParam(FName InKey) : Key(InKey), CustomInfo(FName())
+	{
+	}
+
+	FZCCParam() : Key(FName()), CustomInfo(FName())
+	{
+	}
+
+	/**
+	 * @brief Generally associated to the param handler key
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ACC")
+	FName Key;
+
+	/**
+	 * @brief For additional info
+	 * Example : Type = Morph, but need to know which morph target we want edit , so CustomInfo will be this info
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ACC")
+	FName CustomInfo;
+
+	bool operator!=(const FZCCParam Other) const
+	{
+		return !Equals(Other);
+	}
+
+	bool operator==(const FZCCParam Other) const
+	{
+		return Equals(Other);
+	}
+
+	bool Equals(const FZCCParam Other) const
+	{
+		return (Key == Other.Key && CustomInfo == Other.CustomInfo);
+	}
+
+	bool IsValid() const
+	{
+		return (!Key.IsNone());
+	}
+};
