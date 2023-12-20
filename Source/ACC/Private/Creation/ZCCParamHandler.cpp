fce3adb9d3184862513dea80ed009645d6d567c7
BaTianPig <1326413472@qq.com>
2023-12-20 23:41:47 +0800

    feature:BlueprintNativeEvent

---
+++ Source/ACC/Private/Creation/ZCCParamHandler.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Creation/ZCCParamHandler.h"
+
+bool UZCCParamHandler::OnReceiveInt_Implementation()
+{
+	return true;
+}
---
+++ Source/ACC/Public/Creation/ZCCParamHandler.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ZCCObject.h"
+#include "ZCCParamHandler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACC_API UZCCParamHandler : public UZCCObject
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ZCC|ParamHandler")
+	FName Key;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="ZCC|ParamHandler")
+	bool OnReceiveInt();
+//
+//	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="ZCC|ParamHandler")
+//	bool OnReceiveFloat();
+};
