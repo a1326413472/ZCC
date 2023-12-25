aae16e3f31e0360b8bbb5d490e5d6971e273e435
BaTianPig <1326413472@qq.com>
2023-12-26 00:46:43 +0800

    feature:ZCC Param Handler Data Asset

---
+++ Source/ACC/Public/Creation/ZCCGPHDataAsset.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ZCCGPHDataAsset.generated.h"
+
+/// <summary>
+/// Struct : Param handler Setting
+/// </summary>
+USTRUCT(BlueprintType)
+struct FZCCParamHandlerSetting
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ZCC|Param Handler")
+	FName Key;
+
+
+};
+
+/**
+ * 
+ */
+UCLASS()
+class ACC_API UZCCParamHandlerDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ZCC|Param Handler")
+	TArray<FZCCParamHandlerSetting> HandlerSettings;
+
+};
